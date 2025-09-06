f9dd3da9aa3a26b8b7bc5cac6a294c3e6a9bcfbe
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 06:41:35 +0100

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,14 @@ const eslintConfig = [
   {
     rules: {
       "brace-style": ["warn", "allman"],
-      "quotes": ["warn", "double"]
+      "quotes": ["warn", "double"],
+      "indent": ["warn", 4]
+    }
+  },
+  {
+    files: ["**/*.{mjs,mts}"],
+    rules: {
+      "indent": ["warn", 2]
     }
   }
 ];
